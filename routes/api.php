760c126dc4826bf31df7723c7cf616e2e82aa72b
Julius Duff <8215455@ntc.edu.ph>
2023-04-04 15:59:58 +0800

    Authentication Done

--- routes/api.php
+++ routes/api.php
@@ -5,34 +5,49 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AttendeeController;
+use App\Http\Controllers\AuthController;
+
+/*
+|---------------------
+|    Public Routes  --
+|---------------------
+*/
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+/*
+|--------------------
+| Protected Routes --
+|--------------------
+*/
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/event/attendee/{event_id}', [EventController::class, 'getAttendees']);
+    Route::get('/event', [EventController::class, 'index']);
+    Route::post('/event', [EventController::class, 'store']);
+    Route::put('/event/{event_id}', [EventController::class, 'update']);
+    Route::delete('/event/{event_id}', [EventController::class, 'destroy']);
+    Route::get('/event/{event_id}', [EventController::class, 'show']);
+    Route::post('/attendee', [AttendeeController::class, 'store']);
+});
+
+
+
+
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Testing Routes // TODO: Remove this
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
-Route::get('/event', [EventController::class, 'index']);
-Route::post('/event', [EventController::class, 'store']);
-Route::put('/event/{event_id}', [EventController::class, 'update']);
-Route::delete('/event/{event_id}', [EventController::class, 'destroy']);
-Route::get('/event/{event_id}', [EventController::class, 'show']);
-Route::get('/event/attendee/{event_id}', [EventController::class, 'getAttendees']);
-
-
 Route::get('/student', [StudentController::class, 'index']);
 Route::post('/student', [StudentController::class, 'store']);
 Route::get('/student/{student_id}', [StudentController::class, 'show']);
 Route::delete('/student/{student_id}', [StudentController::class, 'destroy']);
 Route::put('/student/{student_id}', [StudentController::class, 'update']);
-
-Route::post('/attendee', [AttendeeController::class, 'store']);
 Route::get('/attendee', [AttendeeController::class, 'index']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/*
+|--------------------------------------------------------------------------
+| Testing Routes // TODO: Remove this
+|--------------------------------------------------------------------------
+*/
